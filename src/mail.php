b64ee95ea0f80eeab939b096a9b9929f1297a5c2
Zolotukhin Anatoliy <zolotukhinwebpr@mail.ru>
2018-01-30 23:09:48 +0700

    mail php

--- src/mail.php
+++ src/mail.php
@@ -1,11 +1,11 @@
 
 <?php
-$to      = '';
-$subject = 'Заявка с сайта';
-$message = 'Заявка.  Имя: ' . $_POST["name"] . ' Телефон: ' . $_POST["phone"] . ' Email: ' . $_POST["email"] ;
-$headers = 'From: admin@strogo-boots.ru' . "\r\n" .
+$to      = 'email@mail.ru';
+$subject = 'Заявка на консультацию с сайта PETROLAB';
+$message = 'Заявка.  Имя: ' . $_POST["name"] . ' Телефон: ' . $_POST["tel"] . ' Сообщение: ' . $_POST["text"] ;
+$headers = 'From: admin@petrolab.ru' . "\r\n" .
     'Content-Type: text/plain; charset=utf8;' . "\r\n" .
-    'Reply-To: admin@strogo-boots.ru' . "\r\n" .
+    'Reply-To: admin@petrolab.ru' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 mail($to, $subject, $message, $headers);
 ?>
